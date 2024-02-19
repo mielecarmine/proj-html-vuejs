fbb2ce07c08327e7c06d467438aaac2ac34e75fd
mielecarmine <mielecarmine993@gmail.com>
2024-02-19 12:34:37 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <div class="container"></div>
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss";
+</style>
