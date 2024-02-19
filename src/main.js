f1a60af0b241606b2cfdaf96de97b11bb59c3370
mielecarmine <mielecarmine993@gmail.com>
2024-02-19 12:40:33 +0100

    feat: fontawesome

--- src/main.js
+++ src/main.js
@@ -5,4 +5,19 @@ import App from "./App.vue";
 // import boostrapp css
 import "bootstrap/dist/css/bootstrap.css";
 
-createApp(App).mount("#app");
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { faStar as faStarReg } from "@fortawesome/free-regular-svg-icons";
+
+/* add icons to the library */
+library.add(faStar, faStarReg);
+
+const app = createApp(App);
+App.mount("#app");
+app.component("font-awesome-icon", FontAwesomeIcon);
